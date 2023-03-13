fc40b72db36b13f4248258e57933ac4d15c403da
Phil Uebbing <phil.uebbing@newcoursecc.com>
2023-03-13 18:21:27 -0500

    WOrks

---
+++ Visual Studio HTML Assignment/Visual Studio HTML Assignment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Visual_Studio_HTML_Assignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Visual Studio HTML Assignment/Visual Studio HTML Assignment/wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function newColor() {
+    document.getElementById("hello_world").style.color = "blue";
+}
+
+function oldColor() {
+    document.getElementById("hello_world").style.color = "darkolivegreen";
+}
+
+function myMove() {
+    let id = null;
+    const elem = document.getElementById("smallbox");
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + "px";
+        }
+    }
+}
