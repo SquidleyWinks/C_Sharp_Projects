805c1261e36a296e6f8655b97fca4c324d4fbdcd
SquidleyWinks <119633090+SquidleyWinks@users.noreply.github.com>
2023-03-16 18:01:34 -0500

    Acme Accounting

---
+++ AccountingAssignment/AccountingAssignment/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AccountingAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
