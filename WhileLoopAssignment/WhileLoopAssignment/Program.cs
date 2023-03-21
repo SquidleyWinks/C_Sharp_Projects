179a3d8d447e78f0a1e5a1290a0e97b0eadc8908
Phil Uebbing <phil.uebbing@newcoursecc.com>
2023-03-21 16:38:13 -0500

    Loops

---
+++ WhileLoopAssignment/WhileLoopAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoopAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0; // sets counter variable
+            while (i < 10) // condition to perform loop while i less than 10
+            {
+                Console.WriteLine(i); // produce i
+                i++; // increment
+            }
+            Console.ReadLine(); // lets us look at the results
+            int j = 0; // another counter variable
+            do // do/while loop this time
+            {
+                Console.WriteLine(j); // produce j
+                j++; // increment
+            }
+            while (j < 10); // condition down here
+            Console.ReadLine(); // lets us take in the work
+        }
+    }
+}
