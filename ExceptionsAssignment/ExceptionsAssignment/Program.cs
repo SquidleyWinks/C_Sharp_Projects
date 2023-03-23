59227e381c67b314407a5155ba6c64bd4ae3383a
Phil Uebbing <phil.uebbing@newcoursecc.com>
2023-03-23 17:14:29 -0500

    try/catch

--- ExceptionsAssignment/ExceptionsAssignment/Program.cs
+++ ExceptionsAssignment/ExceptionsAssignment/Program.cs
@@ -10,6 +10,34 @@ namespace ExceptionsAssignment
     {
         static void Main(string[] args)
         {
+            try // start try block
+            {
+                List<int> ints = new List<int>() { 22, 24, 30, 32, 36, 40 }; // sets up initial list of numbers
+                Console.WriteLine("Select a number for division"); // prompt for user input
+                int denum = Convert.ToInt32(Console.ReadLine()); // sets variable for user input
+                for (int i = 0; i < ints.Count; i++) // establishes for loop
+                {
+                    int quotient = ints[i] / denum; // divides list index by user input and stores that value in a variable
+                    Console.WriteLine(quotient.ToString()); // displays result to screen
+                }
+            }
+            catch(DivideByZeroException ex) // looks for divide by zero exception
+            {
+                Console.WriteLine(ex.Message); // displays error message
+            }
+            catch(FormatException ex) // looks for wrong format exception
+            {
+                Console.WriteLine(ex.Message); // displays error message
+            }
+            catch(Exception ex) // looks for any other exception
+            {
+                Console.WriteLine(ex.Message); // error message
+            }
+            Console.WriteLine("Emerged from Try/Catch block."); // marks the end of the try/catch block
+            Console.ReadLine();
+            {
+
+            }
         }
     }
 }
