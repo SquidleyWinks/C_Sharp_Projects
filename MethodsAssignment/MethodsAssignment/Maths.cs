de4841b125b7cc27f132fb6d4265d0122b559774
Phil Uebbing <phil.uebbing@newcoursecc.com>
2023-03-24 08:13:39 -0500

    Methods

---
+++ MethodsAssignment/MethodsAssignment/Maths.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment
+{
+    class Maths
+    {
+        public static int AddFive(int n)
+        {
+            n += 5;
+            return n;
+        }
+
+        public static int MultFive(int n)
+        {
+            n *= 5;
+            return n;
+        }
+
+        public static int MinusFive(int n)
+        {
+            n -= 5;
+            return n;
+        }
+    }
+}
---
+++ MethodsAssignment/MethodsAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number for some Maths:");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            userNum = Maths.AddFive(userNum);
+            Console.WriteLine("Result of first method is " + userNum);
+            userNum = Maths.MultFive(userNum);
+            Console.WriteLine("Result of second method is " + userNum);
+            userNum = Maths.MinusFive(userNum);
+            Console.WriteLine("Result of third method is " + userNum);
+            Console.ReadLine();
+
+
+        }
+    }
+}
