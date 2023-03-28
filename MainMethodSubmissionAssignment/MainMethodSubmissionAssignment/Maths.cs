1fdf515894dae818926bfc50d4b2b0b13fc1fa94
Phil Uebbing <phil.uebbing@newcoursecc.com>
2023-03-28 12:40:26 -0500

    overload

---
+++ MainMethodSubmissionAssignment/MainMethodSubmissionAssignment/Maths.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmissionAssignment
+{
+    internal class Maths
+    {
+        public static int AddInt(int i) // first simple method, takes in an int, adds 5, then returns an int
+        {
+            i += 5;
+            return i;
+        }
+
+        public static int AddInt(double d) // first overload of the simple method. Takes in a double, adds 10, then returns as an int
+        {
+            d += 10;
+            return Convert.ToInt32(d);
+        }
+
+        public static int AddInt(string s) // second overload of the simple method. Takes in a string, adds 20, then returns as an int
+        {
+            int num = Convert.ToInt32(s); // make sure to convert before attempting math operations
+            num += 20;
+            return num; // returns new variable as int
+            
+        }
+    }
+}
---
+++ MainMethodSubmissionAssignment/MainMethodSubmissionAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmissionAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:"); // prompts for a number
+            int userNum1 = Convert.ToInt32(Console.ReadLine()); // sets variable converting user input to int
+            Console.WriteLine("The result is " + Maths.AddInt(userNum1)); // calls overloaded method that takes in an int, and displays result
+
+            Console.WriteLine("Please enter a decimal number:"); // prompts for a decimal number
+            double userNum2 = Convert.ToDouble(Console.ReadLine()); // sets variable converting user input to double
+            Console.WriteLine("The result is " + Maths.AddInt(userNum2)); // calls overloaded method that takes in a double and displays result
+
+            Console.WriteLine("Please enter a number:"); // prompts for another number
+            string userNum3 = Console.ReadLine(); // stores value as a string
+            Console.WriteLine("The result is " + Maths.AddInt(userNum3)); // calls overloaded method that takes in a string and displays result
+            Console.ReadLine(); // one last readline so we can see everything on the console
+        }
+    }
+}
