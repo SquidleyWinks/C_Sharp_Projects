c18b023594be323d94a7ee4f7587c7a8fbad5e57
Phil Uebbing <phil.uebbing@newcoursecc.com>
2023-03-28 17:41:22 -0500

    Optional Parameters

---
+++ MethodAssignmentOptionalParameters/MethodAssignmentOptionalParameters/Maths.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignmentOptionalParameters
+{
+    internal class Maths
+    {
+        public static int AddTwo(int a, int b = 5) // adds two integers together, with the second number being option, with a default of 5
+        {
+            int result = a + b;
+            return result;
+        }
+    }
+}
---
+++ MethodAssignmentOptionalParameters/MethodAssignmentOptionalParameters/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignmentOptionalParameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please select a number:"); // prompt for first number
+            int userNum1 = Convert.ToInt32(Console.ReadLine()); // converts user input to int and stores value in a variable
+            Console.WriteLine("You may now select another number, or just press \'Enter\' to select the default."); // prompts user for second, optional number
+            string optionalNum = Console.ReadLine(); // stores the reply as a string, in case they choose the default
+            if(optionalNum != "") // checks the optional number response variable, and executes following code if the user entered their own number
+            {
+                int userNum2 = Convert.ToInt32(optionalNum); // converts the optional variable from string to int
+                Console.WriteLine("The result is: " + Maths.AddTwo(userNum1, userNum2)); // calls our AddTwo method and displays the result to the screen
+            }
+            else // executes if the user opted for the default number value
+            {
+                Console.WriteLine("The result is: " + Maths.AddTwo(userNum1)); // calls AddTwo method with a single input, utilizing the default
+            }
+            Console.ReadLine();
+        }
+    }
+}
