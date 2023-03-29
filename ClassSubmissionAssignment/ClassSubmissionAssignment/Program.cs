691743e23d347272e92db62eec910e9e99988799
Phil Uebbing <phil.uebbing@newcoursecc.com>
2023-03-29 17:46:59 -0500

    update

---
+++ ClassSubmissionAssignment/ClassSubmissionAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmissionAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numHalved = 0; // declaring output parameter variable
+            Console.WriteLine("Please select a number to cut in half"); // prompts user for a number
+            int userNum = Convert.ToInt32(Console.ReadLine()); // sets the number to a variable
+            Maths.CutInHalf(userNum, out numHalved); // performs method using the output parameter
+            Console.WriteLine("The result is: " + numHalved); // displays result to screen
+            Console.ReadLine(); // in place so we have time to see the results
+        }
+    }
+}
