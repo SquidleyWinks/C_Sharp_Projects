cf76f1f5f8c390a171579a10f4acbb64632f25f7
Phil Uebbing <phil.uebbing@newcoursecc.com>
2023-04-02 18:07:19 -0500

    lambda functions

---
+++ LambdaFunctionAssignment/LambdaFunctionAssignment/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaFunctionAssignment
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public FirstNames firstName { get; set; }
+        public LastNames lastName { get; set; }
+
+    }
+    public enum FirstNames // some odd first names for our staff
+    {
+        Joe,
+        Bob,
+        Ned,
+        Ted
+    }
+
+    public enum LastNames // made the properties enums for the sake of easily maintaining the possible names
+    {
+        Bob,
+        Steve,
+        Willy,
+        Billy
+    }
+}
---
+++ LambdaFunctionAssignment/LambdaFunctionAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaFunctionAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Staff staff = new Staff(); // create our staff
+            List<Employee> joeStaff1 = new List<Employee>(); // create a new list for just the Joes
+            foreach (Employee joe in staff.Employees) // foreach loop
+            {
+                if (joe.firstName == FirstNames.Joe) // checks for the first name to be equal to the enum
+                {
+                    joeStaff1.Add(joe); // adds to new list
+                }
+            }
+            List<Employee> joeStaff2 = (List<Employee>)staff.Employees.Where(x => x.firstName == FirstNames.Joe); // the lambda function version is much more compact -- had to add in an extra cast at the start to set the staff object to a list of employees (which it is already, functionally speaking)
+            List<Employee> over5Staff1 = new List<Employee>();
+            foreach(Employee over5 in staff.Employees) // foreach loop
+            {
+                if (over5.Id > 5) // checks if id is over 5
+                {
+                    over5Staff1.Add(over5); // adds to the new list
+                } 
+            }
+            List<Employee> over5Staff2 = (List<Employee>)staff.Employees.Where(x => x.Id > 5); // again, much more compact 
+        }
+    }
+}
