852e983dc012ec8be8e71d533eddc99e9295523e
Phil Uebbing <phil.uebbing@newcoursecc.com>
2023-04-07 07:59:27 -0500

    changes

---
+++ ConstructorAssignment/ConstructorAssignment(.NetFramework)/Chain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment_.NetFramework_
+{
+    internal class Chain
+    {
+        public Chain() : this(6)
+        { 
+
+        }
+        public Chain(int Links) : this(Links, "Padlock") 
+        {
+        }
+        public Chain(int Links, string Locks)
+        {
+            this.Links = Links;
+            this.Locks = Locks;
+        }
+        public int Links { get; set; }
+        public string Locks { get; set; }
+    }
+}
---
+++ ConstructorAssignment/ConstructorAssignment(.NetFramework)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment_.NetFramework_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Hello = "Hello, ";
+            var World = "World!";
+            var chian = new Chain();
+            Console.WriteLine(Hello + World);
+            Console.ReadLine();
+        }
+    }
+}
