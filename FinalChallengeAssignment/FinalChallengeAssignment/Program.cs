8a07b77bbcc42abfd9fb3c9cad35c046a5507455
SquidleyWinks <119633090+SquidleyWinks@users.noreply.github.com>
2023-04-14 17:00:02 -0500

    Code First db

---
+++ FinalChallengeAssignment/FinalChallengeAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+
+namespace FinalChallengeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new SchoolContext())
+            {
+                var student = new Student() { StudentName = "Bob" };
+
+                ctx.Students.Add(student);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
